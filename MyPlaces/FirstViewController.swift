54e0672ac0f169261056e3d853585638e567546d
Albert Mata <hello@albertmata.net>
2018-09-28 11:06:05 +0200

    Initial Commit

---
+++ MyPlaces/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  MyPlaces
+//
+//  Created by Albert Mata Guerra on 28/09/2018.
+//  Copyright © 2018 Albert Mata Guerra. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
