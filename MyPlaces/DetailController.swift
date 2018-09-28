4a48976ef9fc20f41b7e43f5b10649dd4908c8af
Albert Mata <hello@albertmata.net>
2018-09-28 11:43:59 +0200

    PLA1 - Exercici 2

---
+++ MyPlaces/DetailController.swift
@@ -0,0 +1,33 @@
+//
+//  DetailController.swift
+//  MyPlaces
+//
+//  Created by Albert Mata Guerra on 28/09/2018.
+//  Copyright © 2018 Albert Mata Guerra. All rights reserved.
+//
+
+import UIKit
+
+class DetailController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func Close(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
